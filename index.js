61fc14d5df06f396a36683d6be0afb804720aa37
Ender <78042486+EnderKingJ@users.noreply.github.com>
2023-04-17 10:40:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function getProxy(options, callback) {
     navigator.serviceWorker.getRegistrations().then(a=>a.forEach(e=>fetch(e.active.scriptURL).then(e=>e.text()).then(e=>e.includes('croxyproxy')?callback(true):null)))
   }
   
-  if (window.$Rhodium||window.__uv||window.alloyLocation||window.__get$Loc||window._womginx_fetch) {
+  if (window.$Rhodium||window.__uv||window.alloyLocation||window.__get$Loc||window._womginx_fetch||window.$aero) {
     callback(true)
   }
   
